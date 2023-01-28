aba0df22b0468ac15405d4e621eff39127a7366f
NataliaRakoczy <natalia.rakoczy54@gmail.com>
2023-01-28 14:20:22 +0100

    added swiper

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+
+import Swiper, { Pagination } from 'swiper';
+
+
+var swiper = new Swiper(".mySwiper", {
+  modules: [Pagination],
+  loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
