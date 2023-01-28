1ff27d6f91af4c33ec9161fedc1db069bfd7954c
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-01-28 14:54:13 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import './sass/main.scss';
 
 import Swiper, { Pagination } from 'swiper';
 
-
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper('.mySwiper', {
   modules: [Pagination],
   loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
