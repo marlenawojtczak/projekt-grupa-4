a4c0cf123e6accd7d34cb5e886bbf56415b7b60d
NataliaRakoczy <natalia.rakoczy54@gmail.com>
2023-01-28 19:56:16 +0100

    update swiper

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import Swiper, { Pagination } from 'swiper';
 
 var swiper = new Swiper('.mySwiper', {
   modules: [Pagination],
+  // slidesPerVie: 1,
+  // spaceBetween: 10,
   loop: true,
   pagination: {
     el: '.swiper-pagination',
